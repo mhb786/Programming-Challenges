254dbc990c92d6b92fc3f9bc0c6e562951c05e1f
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-14 07:00:26 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Programming Challenge 1/testing1.py
@@ -0,0 +1,12 @@
+import rods
+
+class MyFirstTests(rods.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(hello_world(), 'hello world')
+
+    def test_custom_num_list(self):
+        self.assertEqual(len(create_list(10)), 10)
+
+if __name__ == '__main__':
+    rods.main()
