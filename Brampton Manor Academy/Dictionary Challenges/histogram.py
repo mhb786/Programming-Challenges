fb9e8d8faa41b760bddfe55b8ed0407ba43e1626
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-18 17:31:33 +0100

    Delete Brampton Manor Academy/Dictionary Challenges directory

--- Brampton Manor Academy/Dictionary Challenges/histogram.py
+++
@@ -1,9 +0,0 @@
-word = str(input('Enter word: '))
-
-def check_freq(word):
-    freq = {}
-    for letter in set(word):
-       freq[letter] = word.count(letter)
-    return freq
-
-print(check_freq(word))
