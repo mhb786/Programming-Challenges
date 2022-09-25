a8da7fa7d059dbfe1b686317c3661c8ebf222122
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-25 13:46:04 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Programming Challenge 9/turtleangle.py
@@ -0,0 +1,31 @@
+import turtle
+import math
+
+starting_x = 0
+starting_y = 0
+one_x = int(input('Enter the x coordinate for the first point: '))
+one_y = int(input('Enter the y coordinate for the first point: '))
+two_x = int(input('Enter the x coordinate for the second point: '))
+two_y = int(input('Enter the y coordinate for the second point: '))
+
+turtle.fillcolor('green')
+turtle.title("Fun with Turtle Graphics and Python")
+turtle.write('I will calculate the angle between these two lines')
+
+turtle.goto(one_x, one_y)
+turtle.goto(two_x, two_y)
+
+def calculate_gradient(x1, y1, x2, y2):
+    return (y2 - y1) / (x2 - x1)
+
+def calculate_angle(m2, m1):
+    tan_angle = abs((m2 - m1)) / (1 + m1*m2)
+    angle = math.atan(tan_angle)
+    angle *= 180 / math.pi
+    return angle
+
+gradient1 = calculate_gradient(starting_x, starting_y, one_x, one_y)
+gradient2 = calculate_gradient(one_x, one_y, two_x, two_y)
+
+turtle.write(f'The angle between the two lines is {calculate_angle(gradient2, gradient1)}')
+turtle.done()
