cf5254116368e6bafbb3ad61ee96195b0c990bc9
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-29 21:29:04 +0100

    Update testing2.py

--- Brampton Manor Academy/Programming Challenge 2/testing2.py
+++ Brampton Manor Academy/Programming Challenge 2/testing2.py
@@ -1,12 +1,14 @@
+import unittest
 import richter
 
-class MyFirstTests(unittest.TestCase):
+class TestRichter(unittest.TestCase):
 
-    def test_hello(self):
-        self.assertEqual(hello_world(), 'hello world')
+    def test_joules(self):
+        self.assertEqual(richter.joules(3.4), 7943282347.242789)
 
-    def test_custom_num_list(self):
-        self.assertEqual(len(create_list(10)), 10)
+    def test_tnt(self):
+        self.assertEqual(richter.tnt(7943282347.242789), 1.8984900447521007)
+
 
 if __name__ == '__main__':
     unittest.main()
