fd76e59095bdb47a6ab90731997c8d144e7cdbf8
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-29 21:51:00 +0100

    Update 99_trick.py

--- Brampton Manor Academy/Programming Challenge 3/99_trick.py
+++ Brampton Manor Academy/Programming Challenge 3/99_trick.py
@@ -5,7 +5,7 @@ player_num = int(input('*Player* Pick a number between 50 - 99: '))
 def calculation_result (player_num, factor):
     calc1 = str(player_num + factor)
     calc2 = int(calc1[0]) + int(calc1[1:3])
-    calc3 = player_num - int(calc2)
-    return calc3
+    return player_num - int(calc2)
+
 
 print(f'I said the answer was {answer} and the calculation result was {calculation_result(player_num, factor)}')
