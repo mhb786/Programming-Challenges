61c44c02e04ad5331a5f3c9a1f23b12595e9cfd6
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-29 21:57:04 +0100

    Update testing4.py

--- Brampton Manor Academy/Programming Challenge 4/testing4.py
+++ Brampton Manor Academy/Programming Challenge 4/testing4.py
@@ -1,11 +1,13 @@
-from windchill import calculation
+import unittest
+import windchill
 
-class MyFirstTests(calculation.TestCase):
+class TestWindchill(unittest.TestCase):
 
     def test_calculation(self):
-        self.assertEqual(calculation(10,14), -6.5895344209562525)
-        self.assertEqual(calculation(0,25), -24.093780999553864)
-        self.assertEqual(calculation(-10, 35), -41.16894662953316)
+        self.assertEqual(windchill.calculation(3.5, 18), -16.47884113003356)
+        self.assertEqual(windchill.calculation(10,14), -6.05660500717706)
+        self.assertEqual(windchill.calculation(0,25), -24.093780999553864)
+        self.assertEqual(windchill.calculation(-10, 35), -41.16894662953316)
 
 if __name__ == '__main__':
-    calculation().main()
+    unittest.main()
