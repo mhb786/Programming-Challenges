84741a399eca0dea8e9434fd7c99511b87a350c7
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-29 22:26:10 +0100

    Update testing7.py

---
+++ Brampton Manor Academy/Programming Challenge 7/testing7.py
@@ -0,0 +1,10 @@
+import unittest
+import algo196
+
+class TestAlgorithm(unittest.TestCase):
+
+    def test_algorithm(self):
+        self.assertEqual(algo196.palindrome(), )
+
+if __name__ == '__main__':
+    unittest.main()
