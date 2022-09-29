402eaacc468eade9d884f3714483e47953d6eeed
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-09-29 22:11:37 +0100

    Update testing5.py

--- Brampton Manor Academy/Programming Challenge 5/testing5.py
+++ Brampton Manor Academy/Programming Challenge 5/testing5.py
@@ -1,17 +1,12 @@
-Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import unittest
+import slayer
 
-from mycode import *
+class TestSlayer(unittest.TestCase):
 
+    def test_slayer(self):
+        self.assertEqual(slayer.answer(142857), 428571)
+        self.assertEqual(slayer.layers(142857), 428571)
 
-class MyFirstTests(unittest.TestCase):
-
-    def test_hello(self):
-        self.assertEqual(hello_world(), 'hello world')
-
-    def test_custom_num_list(self):
-        self.assertEqual(len(create_list(10)), 10)
 
 if __name__ == '__main__':
     unittest.main()
