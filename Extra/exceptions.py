d23d59070cc7338e9d4f3aed8aaf53c56813d96a
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-10-14 16:50:58 +0100

    Update exceptions.py

--- Extra/exceptions.py
+++ Extra/exceptions.py
@@ -3,19 +3,25 @@ ops = { "+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.t
 
 def operations():
     table = []
-    # inputop = input("Please enter an operator (+, -, *, /): ")
-    # inputnum = int(input("Please enter a natural number: "))
-    inputop = '+'
-    inputnum= int('4')
+    inputop = input("Please enter an operator (+, -, *, /): ")
+    inputnum = int(input("Please enter a natural number: "))
     for number in range(-1, inputnum+1):
         row = [0]*(inputnum+1)
         table.append([number, row])
     table[0][0] = inputop
     table[0][1] = list(range(inputnum+1))
-    for digit in table[2][1]:
-        digit = 1000
-        return table
-
-
+    index = 0
+    topnum = 0
+    n = 1
+    while n <= inputnum+1:
+        while topnum < inputnum+1 and index < inputnum+1:
+            table[n][1][index] = ops[inputop](table[0][1][topnum], table[n][0])
+            index+=1
+            topnum+=1
+        index = 0
+        topnum = 0
+        n+=1
+    for row in table:
+        print(*row)
 
 print(operations())
