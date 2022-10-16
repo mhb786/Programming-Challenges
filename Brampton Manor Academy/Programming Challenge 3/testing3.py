e3d7b498a43a3faaccc016a8c8d316044b318c10
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-10-16 10:55:11 +0100

    Update testing3.py

--- Brampton Manor Academy/Programming Challenge 3/testing3.py
+++ Brampton Manor Academy/Programming Challenge 3/testing3.py
@@ -7,6 +7,11 @@ class Testing99Trick(unittest.TestCase):
     def test_calculation(self):
         self.assertEqual(trick99.calculation_result(72, 84), 15)
 
+        self.assertEqual(trick99.calculation_result(97, 84), 15)
+
+        self.assertEqual(trick99.calculation_result(57, 84), 15)
+
+
 
 if __name__ == '__main__':
     unittest.main()
