8dec36b09f8a9eb2639b41f67c82e442b886c86d
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-10-20 08:05:45 +0100

    Update minion_game.py

--- Extra/Minion Game/minion_game.py
+++ Extra/Minion Game/minion_game.py
@@ -1,34 +1,38 @@
+from time import sleep
+
 def check_string():
-    while (True):
+    while True:
         try:
             string = input('Enter a word: ')
             if string.isalpha() == False:
                 raise ValueError
             break
         except ValueError:
+            sleep(1)
             print("Invalid input, try again")
     return string
 
 
 def find_winner(string):
     length = len(string)
-    stuart_points = 0
-    kevin_points = 0
+    p1 = 0
+    p2 = 0
 
-    for i in range(length):
-        if string[i].lower() in 'aeiou':
-            kevin_points += (length - i)
+    for count, value in enumerate(string):
+        if value.lower() in 'aeiou':
+            p2 += (length - count)
         else:
-            stuart_points += (length - i)
+            p1 += (length - count)
 
-    return stuart_points, kevin_points
+    return p1, p2
 
 
 def output(p1, p2):
+    sleep(2)
     if p1 > p2:
         print("\033[1m" + 'Stuart ' + str(p1) + "\033[0m")
     elif p2 > p1:
-        print("\033[1m" + 'Stuart ' + str(p2) + "\033[0m")
+        print("\033[1m" + 'Kevin ' + str(p2) + "\033[0m")
     else:
         print("\033[1m" + 'Draw' + "\033[0m")
 
