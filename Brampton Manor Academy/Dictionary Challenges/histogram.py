69c7037dc8a099b1b6abcfd243057f0adf88a25c
Muhibur Rahman <mrmuhibur47@gmail.com>
2022-11-19 16:27:10 +0000

    Delete Brampton Manor Academy/Dictionary Challenges directory

--- Brampton Manor Academy/Dictionary Challenges/histogram.py
+++
@@ -1,9 +0,0 @@
-word = str(input('Enter word: '))
-
-def histogram(word):
-    hist = {}
-    for letter in set(word):
-       hist[letter] = word.count(letter)
-    return hist
-
-print(histogram(word))
