3641b1e6b93d734ef2a1f06736f68fed85372d23
Muhibur Rahman <mrmuhibur47@gmail.com>
2023-03-01 11:27:26 +0000

    Update tic_tac_toe_ai_lesson_1.py

--- Brampton Manor Academy/Tic Tac Toe/tic_tac_toe_ai_lesson_1.py
+++ Brampton Manor Academy/Tic Tac Toe/tic_tac_toe_ai_lesson_1.py
@@ -13,13 +13,14 @@ def empty_cells(board):
     empty = []
     for Column in range(3):
         for Row in range(3):
-            if board[Column][Row] == '':
+            if board[Column][Row] == ' ':
                 empty.append([Column, Row])
 
     return empty
 
 
 def player_turn(player_sign, board, empty_cells):
+    print("Player 1 turn")
     br = True
     while br:
         turn = input("Enter a coordinate: ")
@@ -38,39 +39,51 @@ def player_turn(player_sign, board, empty_cells):
 
 
 def ai_turn(player_sign, board, empty_cells):
-    br = True
-    while br:
-        try:
-          turn = [random.randint(0, 2), random.randint(0, 2)]
-          Column = int(turn[0])
-          Row = int(turn[1])
-
-          if Column and Row not in range(3):
-              raise Exception
-          elif [Column, Row] in empty_cells:
-              br = False
-          else:
-              raise Exception
-        except Exception:
-          print()
+    print("AI turn")
+    choice = random.choice(empty_cells)
+    Column, Row = choice[0], choice[1]
 
     board[Column][Row] = player_sign
-
     return board
 
 
 def CheckBoard(board):
-    pass
+    for row in board:
+        if len(set(row)) == 1 and row[0] != ' ':
+            print("You are the winner")
+            return row[0]
+
+    for Column in range(3):
+        x = set([board[Column][Row] for Row in range(3)])
+        print(x)
+        if len(x) == 1 and x[0] != ' ':
+            print("You are the winner")
+            return x[0]
+
+
+
+    # if len(set([board[i][i] for i in range(len(board))])) == 1:
+    #     print("yay")
+    #     return board[0][0]
+    # if len(set([board[i][len(board) - i - 1] for i in range(len(board))])) == 1:
+    #     print("yay")
+    #     return board[0][len(board) - 1]
+
+    return 0
+
 
 def reset_board():
-    board = [['' for Column in range(3)] for Row in range(3)]
+    board = [[' ' for Column in range(3)] for Row in range(3)]
     return board
 
 
 def display_board(board):
-    for i in board:
-        print(i)
-    print('\n')
+    str_line = '---------------'
+    print('\n' + str_line)
+    for row in board:
+        for cell in row:
+            print(f'| {cell} |', end='')
+        print('\n' + str_line)
 
 
 def main():
@@ -81,15 +94,19 @@ def main():
     Finished = False
 
     while not Finished:
-        print("Player 1 turn")
         player_turn(PLAYER, board, empty)
         empty = empty_cells(board)
         display_board(board)
+        if CheckBoard(board):
+            break
 
-        print("AI turn")
         ai_turn(AI, board, empty)
         empty = empty_cells(board)
         display_board(board)
+        if CheckBoard(board):
+            break
 
+    print("Game has ended")
 
 main()
+
